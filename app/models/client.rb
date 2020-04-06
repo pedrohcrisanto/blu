c72e5c018a73ad55aa39827df2ec7aa77bc8d56a
Pedro Crisanto <pedrohcrisanto@gmail.com>
2020-04-06 12:07:05 -0300

    Read TXT file

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,15 @@ class Client < ApplicationRecord
     has_many :transactions
     accepts_nested_attributes_for :transactions, allow_destroy: true
     has_one_attached :file
+    def file_path
+        ActiveStorage::Blob.service.path_for(file.key)
+      end
+
+
+    def read_txt
+        File.read( self.file_path ) do |line|
+            puts line
+          end
+    end
+
 end
